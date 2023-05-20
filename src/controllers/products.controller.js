b4aa0df8a5f5562e663f9a57e0e00e73fba731b1
antony-chi <antony.cp9@gmail.com>
2023-05-20 14:00:48 -0500

    se preparo CRUD product.route

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -3,7 +3,7 @@ export const createProduct = (req, res) => {
 }
 
 export const getProducts = (req, res) => {
-    
+    res.json({code:200, message: 'get products'})
 }
 
 export const getProductsById = (req, res) => {
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,6 +4,15 @@ const router = Router()
 //importamos controller
 import * as productsCtrl from '../controllers/products.controller.js'
 
+router.post('/', productsCtrl.createProduct)
+
 router.get('/', productsCtrl.getProducts)
+
+router.get('/:productId', productsCtrl.getProductsById)
+
+router.put('/:productId', productsCtrl.updateProductById)
+
+router.delete('/:productId', productsCtrl.deletetProductById)
+
 
 export default router;
